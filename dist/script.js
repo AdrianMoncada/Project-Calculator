cf791731b547a45d32748f7c77f9149ca227ea40
AdrianMoncada <adrian.ignaciomoncada@gmail.com>
2021-08-26 21:06:27 -0300

    adding styles to the calculator using mixin with a minimalistic aproach

---
+++ dist/script.js
@@ -0,0 +1,7 @@
+let displayValue = '0';
+let firstOperand = null;
+let secondOperand = null;
+let firstOperator = null;
+let secondOperator = null;
+let result = null;
+const buttons = document.querySelectorAll('button');
